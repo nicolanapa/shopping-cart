c3ca983c4d2ede9759507d521107d799fc5362d6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-05 18:07:05 +0200

    Added main config files for eslint and vite

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	{ files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+	pluginReactConfig,
+	{
+		rules: {
+			"no-unused-vars": "warn",
+			"react/prop-types": "off",
+		},
+	},
+];
+
