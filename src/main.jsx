b90aa6a07575799c9465b1efd3fba2f38eb1a3b5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-06 10:27:16 +0200

    Added comment to use RouterProvider next

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Header />
+		{/*RouterProvider*/}
 	</React.StrictMode>
 );
 
