2c796644869539ce03e169b4be74197af09545d5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-06 12:17:59 +0200

    Imported the necessary react-router-dom functions and created the BrowserRouter variable

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./style.css";
 import { Header } from "./components/Header";
+import { routes } from "./routes";
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Header />
-		{/*RouterProvider*/}
+		<RouterProvider router={router}></RouterProvider>
 	</React.StrictMode>
 );
