e8481d54a1b4ba5ec28f1109e2e416dbcde6dadc
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-06 10:21:20 +0200

    Added the Header Component at the top

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+import { Header } from "./components/Header";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Header />
+	</React.StrictMode>
+);
 
