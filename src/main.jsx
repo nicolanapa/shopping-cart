5d01af9cd7e837dde0ce25a3fc436a442401f714
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-06 12:21:09 +0200

    Updated the path of style.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./style.css";
+import "./styles/style.css";
 import { Header } from "./components/Header";
 import { routes } from "./routes";
 
