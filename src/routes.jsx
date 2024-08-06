1f468368a3de59ba425eab7be7f44d64b8c2c7bc
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-06 12:17:09 +0200

    Added routes const with two paths and exported it

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,1 +1,18 @@
 import React from "react";
+import { Home } from "./components/Home";
+import { Shop } from "./components/Shop";
+import { ErrorPage } from "./components/ErrorPage";
+
+const routes = [
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "shop",
+		element: <Shop />,
+	},
+];
+
+export { routes };
