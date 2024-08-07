a762dae3b42beb58fe7033a654c41c51e0ad76e0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-07 14:11:22 +0200

    Added a test to see if the Header get displayed

---
+++ src/tests/routes.test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
+import { Header } from "../components/Header";
+
+describe("Testing that the Router", () => {
+	it("displays the Header", () => {
+		render(<Header />, { wrapper: BrowserRouter });
+		expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+		expect(screen.getByRole("link", { name: "Shop" })).toBeInTheDocument();
+	});
+});
