c61bf817859811c2fa6e87182555a6f43e7fe4c7
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-07 13:42:28 +0200

    Added simple header in Header() with the Link Component pointing to the specific href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Header() {}
+function Header() {
+	return (
+		<header>
+			<h1>
+				<Link to="/">Home</Link>
+			</h1>
+			<h2>
+				<Link to="shop">Shop</Link>
+			</h2>
+		</header>
+	);
+}
 
 export { Header };
