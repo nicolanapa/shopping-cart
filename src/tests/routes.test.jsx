8a1ccd7fb4744c1d2be48c562af460cf19f6fd5a
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-07 15:28:16 +0200

    Fixed the React Router way of rendering not displaying any other Elements than Header

--- src/tests/routes.test.jsx
+++ src/tests/routes.test.jsx
@@ -1,17 +1,93 @@
 import React from "react";
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { getAllByRole, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, MemoryRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import { Header } from "../components/Header";
+import { Home } from "../components/Home";
+import { Shop } from "../components/Shop";
 
 describe("Testing that the Router", () => {
 	it("displays the Header", () => {
-		render(<Header />, { wrapper: BrowserRouter });
+		render(
+			<MemoryRouter initialEntries={["/"]}>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+				</Routes>
+			</MemoryRouter>
+		);
 
 		screen.debug();
 
 		expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
 		expect(screen.getByRole("link", { name: "Shop" })).toBeInTheDocument();
+	});
+
+	it("displays the Home at default", () => {
+		render(
+			<MemoryRouter initialEntries={["/"]}>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		screen.debug();
+
+		expect(screen.getByRole("heading", { level: 2, name: "Test Home" })).toBeInTheDocument();
+	});
+
+	it("displays the Home at /home", () => {
+		render(
+			<MemoryRouter initialEntries={["/home"]}>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		screen.debug();
+
+		expect(screen.getByRole("heading", { level: 2, name: "Test Home" })).toBeInTheDocument();
+	});
+
+	it("displays the /shop and /home correctly", async () => {
+		const user = userEvent.setup();
+
+		render(
+			<MemoryRouter initialEntries={["/"]}>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/shop" element={<Shop />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
+		screen.debug();
+
+		expect(screen.getByRole("heading", { level: 2, name: "Test Home" })).toBeInTheDocument();
+
+		const headerLinks = [screen.getByRole("link", { name: "Home" }), screen.getByRole("link", { name: "Shop" })];
+
+		await user.click(headerLinks[1]);
+
+		screen.debug();
+
+		expect(screen.getByRole("heading", { level: 2, name: "Test Shop" })).toBeInTheDocument();
+
+		await user.click(headerLinks[0]);
+
+		expect(screen.getByRole("heading", { level: 2, name: "Test Home" })).toBeInTheDocument();
 	});
 });
