ad387281991595e7fc8eeb03ad4dd2e379f1f961
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-07 13:46:58 +0200

    Removed the Header tag calling from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/style.css";
-import { Header } from "./components/Header";
 import { routes } from "./routes";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Header />
 		<RouterProvider router={router}></RouterProvider>
 	</React.StrictMode>
 );
