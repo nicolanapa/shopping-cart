e06b027469679c5650002577a995e3b5b412502e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-07 14:17:38 +0200

    Imported Outlet from react-router-dom and pushed it at the bottom of the fragment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Header() {
 	return (
-		<header>
-			<h1>
-				<Link to="/">Home</Link>
-			</h1>
-			<h2>
-				<Link to="shop">Shop</Link>
-			</h2>
-		</header>
+		<>
+			<header>
+				<h1>
+					<Link to="/">Home</Link>
+				</h1>
+				<h2>
+					<Link to="shop">Shop</Link>
+				</h2>
+			</header>
+			<Outlet />
+		</>
 	);
 }
 
