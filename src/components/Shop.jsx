cc789c51e331cf13d098bdc216cc0d0602d15efc
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 12:00:28 +0200

    Removed 'Test' Word from the h2 and added .title .center class to it

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Shop() {
-	return <main><h2>Test Shop</h2></main>;
+	return (
+		<main>
+			<h2 className="title center">Shop</h2>
+		</main>
+	);
 }
 
 export { Shop };
