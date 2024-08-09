8de1096f35c502b38c651be4bd4e40a4d9191877
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 14:20:19 +0200

    Fixed the Test expecting

--- src/components/fetchProduct.js
+++ src/components/fetchProduct.js
@@ -1,3 +1,18 @@
-function fetchProduct() {}
+async function fetchProduct(quantity = "?limit=15", specificProduct = "") {
+	let query;
+
+	if (quantity === "?limit=15") {
+		query = await fetch("https://fakestoreapi.com/products" + quantity);
+		query = await query.json();
+	} else if (quantity === Number) {
+		query = await fetch("https://fakestoreapi.com/products?limit=" + quantity);
+		query = await query.json();
+	} else if (quantity === "" && specificProduct !== "") {
+		query = await fetch("https://fakestoreapi.com/products/" + specificProduct);
+		query = await query.json();
+	}
+
+	return query;
+}
 
 export { fetchProduct };
