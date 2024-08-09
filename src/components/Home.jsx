1bd5f91ee514241327c2192df8b1fcab4e7b68ff
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 11:13:24 +0200

    Added a Container for the main Informations/News

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,17 @@ import React from "react";
 function Home() {
 	return (
 		<main>
-			<h2>Test Home</h2>
+			<h2>Home</h2>
+			<div className="grid-home-container">
+				<section className="information-container rose-color">
+					<h3>Launched in 2024</h3>
+					<p>15 Products are available</p>
+				</section>
+				<section className="information-container green-color">
+					<h3>Collectors Edition coming soon</h3>
+					<p>Yes, that&apos;s right!</p>
+				</section>
+			</div>
 		</main>
 	);
 }
