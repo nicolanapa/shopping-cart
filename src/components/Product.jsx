4745d4c1b3fae1fb3ffeaf2c9d0b9107c566a7fe
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 14:49:51 +0200

    Added first version of Product's HTML Code without the stars

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../styles/product.css";
 
 function Product({ product }) {
-	return <div></div>;
+	return (
+		<div className="product-container">
+			<p className="category">{product.category}</p>
+			{/*Stars and rating count*/}
+			<h3 className="product-title">{product.title}</h3>
+			<p>{product.description}</p>
+
+			<h4>{product.price}</h4>
+
+			<div className="button-container">
+				<input id="quantity-to-cart" type="number" min="1" max="30" />
+				<button>Add To Cart</button>
+			</div>
+
+			<img src={product.image} alt={product.title} width="100px" height="auto" />
+		</div>
+	);
 }
 
 export { Product };
