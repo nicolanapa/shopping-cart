e6a54e82a5ffc8ca1fd0e0bc5670541b4490ce06
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 14:34:42 +0200

    Added test which checks if the first Product gets rendered correctly

---
+++ src/tests/Product.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Product } from "../components/Product";
+import { fetchProduct } from "../components/fetchProduct";
+
+describe("Testing that Product", () => {
+	const product1 = {
+		id: 1,
+		title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+		price: 109.95,
+		description:
+			"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+		category: "men's clothing",
+		image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+		rating: { rate: 3.9, count: 120 },
+	};
+
+	it("get rendered correctly", async () => {
+		render(<Product product={await fetchProduct("", 1)} />);
+
+		screen.debug();
+
+		expect(screen.getByRole("heading", { level: 3, name: product1.name })).toBeInTheDocument();
+		expect(screen.getByRole("heading", { level: 4, name: product1.price })).toBeInTheDocument();
+		expect(screen.getByText(product1.description)).toBeInTheDocument();
+	});
+});
