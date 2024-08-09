d4c87040fd25945a0ef3a3dd7b2f054134f28fa1
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 13:26:35 +0200

    Added a basic variable that query fetchProduct() to all the tests

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -1,5 +1,16 @@
 import { describe, it } from "vitest";
+import { fetchProduct } from "../components/fetchProduct";
 
 describe("Testing that fetchProduct", () => {
-	it("fetches and returns a Product", async () => {});
+	it("fetches and returns a Product", async () => {
+		const productQuery = await fetchProduct(1);
+	});
+
+	it("fetches and returns a specific Product", async () => {
+		const productQuery = await fetchProduct(1, 14);
+	});
+
+	it("fetches and returns 15 Products", async () => {
+		const productQuery = await fetchProduct();
+	});
 });
