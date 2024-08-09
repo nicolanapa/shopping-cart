51899ce320c8eb487e89397ff657a1dcf2008ec2
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-09 16:49:08 +0200

    Added two States to fetch and render the Products

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchProduct } from "./fetchProduct";
+import { Product } from "./Product";
+import { v7 as uuidv7 } from "uuid";
 
 function Shop() {
+	const [allProducts, setAllProducts] = useState([]);
+	const [allRenderedProducts, setAllRenderedProducts] = useState([]);
+
+	useEffect(() => {
+		async function fetchEverythingThenRender() {
+			let tempAllProducts = await fetchProduct();
+			setAllProducts(tempAllProducts);
+		}
+
+		fetchEverythingThenRender();
+	}, []);
+
+	useEffect(() => {
+		if (allProducts.length > 0) {
+			console.log("EVERYTHING", allProducts);
+			let tempAllRenderedProducts = allRenderedProducts.splice();
+
+			for (let i = 0; i < 15; i++) {
+				let randomKey = uuidv7();
+
+				if (tempAllRenderedProducts.length === 0) {
+					setAllRenderedProducts(<Product product={allProducts[i]} key={randomKey} />);
+					tempAllRenderedProducts.push(<Product product={allProducts[i]} key={randomKey} />);
+				} else {
+					tempAllRenderedProducts.push(<Product product={allProducts[i]} key={randomKey} />);
+				}
+			}
+
+			setAllRenderedProducts(tempAllRenderedProducts);
+			console.log("TO RENDER", allRenderedProducts);
+		}
+	}, [allProducts]);
+
 	return (
 		<main>
-			<h2 data-testid="title" className="title center">Shop</h2>
+			<h2 data-testid="title" className="title center">
+				Shop
+			</h2>
+
+			<section className="all-products-container">{allRenderedProducts}</section>
 		</main>
 	);
 }
