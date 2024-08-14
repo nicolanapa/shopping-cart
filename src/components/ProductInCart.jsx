3132bce5697bf67bbbb758ee1f02d603e5fe175c
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-14 13:05:56 +0200

    Added the structure of the Single Product in ProductInCart()

--- src/components/ProductInCart.jsx
+++ src/components/ProductInCart.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function ProductInCart({ product }) {
-	return <div></div>;
+	return (
+		<div className="product-in-cart-container">
+			<h4>{product.title}</h4>
+
+			<div>
+				<p>{product.amount}</p>
+				<p>x</p>
+				<h5>{product.price} €</h5>
+			</div>
+		</div>
+	);
 }
 
 export { ProductInCart };
