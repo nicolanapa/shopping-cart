2751b17355f68a8488e1e51218f98cc99ad7f960
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-14 13:05:14 +0200

    Added amount State which calculates the Checkout Amount

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { ProductInCart } from "./ProductInCart";
 import { v7 as uuidv7 } from "uuid";
 
 function Cart() {
 	const [productsInCart, setProductsInCart] = useOutletContext();
+	const [amount, setAmount] = useState(0);
+
+	for (let i = 0; i < productsInCart.length; i++) {
+		setAmount(setAmount + productsInCart[i].price * productsInCart[i].amount);
+	}
 
 	return (
-		<section>
+		<section className="cart">
 			<h3>Here&apos;s your Cart</h3>
 
 			{productsInCart.length === 0 ? (
@@ -16,7 +21,16 @@ function Cart() {
 				productsInCart.map((product) => {
 					let randomKey = uuidv7();
 
-					return <ProductInCart product={product} key={randomKey} />;
+					return (
+						<div className="products-in-cart-container" key={randomKey}>
+							<ProductInCart product={product} />
+
+							<div>
+								<p>{amount} €</p>
+								<button>Pay Now!</button>
+							</div>
+						</div>
+					);
 				})
 			)}
 		</section>
