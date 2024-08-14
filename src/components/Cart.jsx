55235949b059cb407ae15adf7d9e211b5312964b
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-14 12:42:06 +0200

    Added productsInCart State with not yet done mapping of it

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { useOutletContext } from "react-router-dom";
 function Cart() {
+	const [productsInCart, setProductsInCart] = useOutletContext();
+
 	return (
 		<section>
 			<h3>Here&apos;s your Cart</h3>
+
+			{productsInCart.length === 0 ? <p>Nothing here yet!</p> : productsInCart.map((product) => {})}
 		</section>
 	);
 }
