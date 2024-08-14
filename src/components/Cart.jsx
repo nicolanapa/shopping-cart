a90566b95cfbf3b34bb4828522d9c98a54d33143
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-14 12:53:54 +0200

    Added the remaining code of productsInCart.map with the returning of ProductInCart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
+import { ProductInCart } from "./ProductInCart";
+import { v7 as uuidv7 } from "uuid";
+
 function Cart() {
 	const [productsInCart, setProductsInCart] = useOutletContext();
 
@@ -7,7 +10,15 @@ function Cart() {
 		<section>
 			<h3>Here&apos;s your Cart</h3>
 
-			{productsInCart.length === 0 ? <p>Nothing here yet!</p> : productsInCart.map((product) => {})}
+			{productsInCart.length === 0 ? (
+				<p>Nothing here yet!</p>
+			) : (
+				productsInCart.map((product) => {
+					let randomKey = uuidv7();
+
+					return <ProductInCart key={randomKey} product={product} />;
+				})
+			)}
 		</section>
 	);
 }
