9625b547a7570ea5b71cf5d3000383967887c451
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-15 17:34:18 +0200

    Moved the Test in another correctly named test and kept the part about testing only one added Product

--- src/tests/Cart.test.jsx
+++ src/tests/Cart.test.jsx
@@ -73,6 +73,29 @@ describe("Testing that the Cart", () => {
 		expect(screen.getByTestId("amount-of-product")).toHaveTextContent(1);
 		expect(screen.getByTestId("total-amount-in-cart")).toHaveTextContent("109.95 €");
 
+		screen.debug();
+	});
+
+	it("shows up two same Products added in one .product-in-cart-container", async () => {
+		const user = userEvent.setup();
+
+		render(<RouterProvider router={router}></RouterProvider>);
+
+		await user.click(screen.getByRole("link", { name: "Shop" }));
+		await user.click(screen.getByAltText("Cart"));
+
+		expect(screen.queryByRole("heading", { level: 3, name: "Here's your Cart" })).toBeInTheDocument();
+
+		let addButtons = await screen.findAllByRole("button", { name: "Add" });
+		let addCartButtons = await screen.findAllByRole("button", { name: "Add To Cart" });
+
+		await user.click(addButtons[0]);
+		await user.click(addCartButtons[0]);
+
+		expect(screen.getAllByTestId("product-in-cart")).toHaveLength(1);
+		expect(screen.getByTestId("amount-of-product")).toHaveTextContent(1);
+		expect(screen.getByTestId("total-amount-in-cart")).toHaveTextContent("109.95 €");
+
 		await user.click(addButtons[0]);
 		await user.click(addCartButtons[0]);
 
