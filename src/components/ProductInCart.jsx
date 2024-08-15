70cdd95a2cdd6e640de1e92038310cbb0f142ed0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-15 19:48:42 +0200

    Added processedCategoryImage which selects the right category Image and displays it

--- src/components/ProductInCart.jsx
+++ src/components/ProductInCart.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { v7 as uuidv7 } from "uuid";
 
 function ProductInCart({ product }) {
+	const [processedCategoryImage, setProcessedCategoryImage] = useState();
+
+	useEffect(() => {
+		let firstUppedCategoryName = product.category.charAt(0).toUpperCase() + product.category.slice(1);
+
+		if (firstUppedCategoryName === "Men's clothing") {
+			setProcessedCategoryImage(
+				<img
+					className="cart-category-image"
+					src={"/shop/categories/clothing.svg"}
+					alt={firstUppedCategoryName}
+					width="25px"
+					height="auto"
+				/>
+			);
+		} else if (firstUppedCategoryName === "Women's clothing") {
+			setProcessedCategoryImage(
+				<img
+					className="cart-category-image"
+					src={"/shop/categories/clothing.svg"}
+					alt={firstUppedCategoryName}
+					width="25px"
+					height="auto"
+				/>
+			);
+		} else {
+			setProcessedCategoryImage(
+				<img
+					className="cart-category-image"
+					src={"/shop/categories/" + product.category + ".svg"}
+					alt={firstUppedCategoryName}
+					width="25px"
+					height="auto"
+				/>
+			);
+		}
+	}, [product]);
+
 	return (
 		<div data-testid="product-in-cart" className="product-in-cart-container">
-			<h4>{product.title}</h4>
+			<h4>
+				{processedCategoryImage}
+				&nbsp;
+				{product.title}
+			</h4>
 
 			<div className="amount-container">
 				<p data-testid="amount-of-product">{product.amount}</p>
