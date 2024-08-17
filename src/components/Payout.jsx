3cd328412b60c25b676618c6a643a048d230ee6d
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-08-17 10:17:58 +0200

    Added some test display to see if it's working and added the amount State via Outlet

--- src/components/Payout.jsx
+++ src/components/Payout.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
-function Payout() {}
+function Payout() {
+	const [amount, setAmount] = useOutletContext();
+
+	return (
+		<section>
+			<p>Test Payment -&gt; {amount} €</p>
+		</section>
+	);
+}
 
 export { Payout };
